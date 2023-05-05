5be06e6f77f4739722cee36742669e8e112bbd1a
alshkre9 <polyex9@gmail.com>
2023-05-05 17:32:38 +0200

    establish the basic building blocks for the main page

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from app.__init__ import app
 from app.functions import login_required
-from flask import redirect, session, url_for, render_template
+from flask import redirect, session, url_for, render_template, request
 
 @app.route("/")
 @login_required
 def main_page():
-	return render_template("main_page.html")
+	return render_template("main_base.html", app_name="main", path=f"{url_for('search')}")
+
+@app.route("/search")
+@login_required
+def search():
+	return str(request.values["q"])
