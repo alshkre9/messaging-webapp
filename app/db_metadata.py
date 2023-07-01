63b9bae761ebaaa8531a122caa091c3a75678aeb
alshkre9 <polyex9@gmail.com>
2023-07-01 12:34:27 +0200

    change data base model

--- app/db_metadata.py
+++ app/db_metadata.py
@@ -1,15 +1,11 @@
 from sqlalchemy.sql import func
-from sqlalchemy import create_engine, Column, Integer, VARCHAR, ForeignKey, Enum, Text, DateTime
+from sqlalchemy import create_engine, Column, Integer, VARCHAR, ForeignKey, Text, DateTime
 from sqlalchemy.orm import DeclarativeBase
 
 from os import environ
-import enum
 
 password = environ.get('mysql_password')
 ENGINE = create_engine(f"mysql://root:{password}@localhost:3306/website")
-
-class Room_type(enum.Enum):
-    User = "User"
 
 class Base(DeclarativeBase):
     pass
@@ -21,13 +17,6 @@ class User(Base):
     username = Column("username", VARCHAR(28), nullable=False)
     hash = Column("hash", Text, nullable=False)
     filename = Column("filename", VARCHAR(30), nullable=False, default="default.jpg")
-    room_id = Column("room_id", Integer, ForeignKey("rooms.id"), nullable=False)
-
-class Room(Base):
-    __tablename__ = "rooms"
-
-    id = Column("id", Integer, primary_key=True)
-    model = Column("model", VARCHAR(12), Enum(Room_type), nullable=False) 
 
 class Friendship(Base):
     __tablename__ = "friendships"
@@ -35,14 +24,13 @@ class Friendship(Base):
     id = Column("id", Integer, primary_key=True)
     user_id = Column("user_id", Integer, ForeignKey("users.id"), nullable=False)
     friend_id = Column("friend_id", Integer, ForeignKey("users.id"), nullable=False)
-    friend_room = Column("room_id", Integer, ForeignKey("rooms.id"), nullable=False)
 
 class Message(Base):
     __tablename__ = "messages"
 
     id = Column("id", Integer, primary_key=True)
-    from_room = Column("from_room", Integer, ForeignKey("rooms.id"), nullable=False)
-    to_room = Column("to_room", Integer, ForeignKey("rooms.id"), nullable=False)
+    from_ = Column("from_room", Integer, ForeignKey("users.id"), nullable=False)
+    to = Column("to_room", Integer, ForeignKey("users.id"), nullable=False)
     value = Column("value", Text, nullable=False)
     date = Column("date", DateTime, server_default=func.now())
 
